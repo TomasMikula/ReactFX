0ea8d44a0c392ac1843f698f99ba8009b7b73de6
Tomas Mikula <tomas.mikula@gmail.com>
2014-01-30 21:50:18 +0100

    Renaming.

---
+++ src/main/java/reactfx/EventStream.java
@@ -0,0 +1,32 @@
+package reactfx;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+/**
+ * Stream of values (events).
+ *
+ * It is an analog of rxJava's {@code Observable}, but "Observable"
+ * already has a different meaning in JavaFX.
+ *
+ * @param <T> type of values this source emits.
+ */
+public interface EventStream<T> {
+
+    /**
+     * Get notified every time this stream emits a value.
+     * @param consumer function to call on the emitted value.
+     * @return subscription that can be used to stop observing
+     * this event stream.
+     */
+    Subscription subscribe(Consumer<T> consumer);
+
+    default EventStream<T> filter(Predicate<T> predicate) {
+        return EventStreams.filter(this, predicate);
+    }
+
+    default <U> EventStream<U> map(Function<T, U> f) {
+        return EventStreams.map(this, f);
+    }
+}
--- src/main/java/reactfx/EventStreams.java
+++ src/main/java/reactfx/EventStreams.java
@@ -9,7 +9,7 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 
 
-public class Sources {
+public class EventStreams {
 
     public static interface Combinator2<A, B, R> {
         R combine(A a, B b);
@@ -25,128 +25,128 @@ public class Sources {
 
     /**
      * Type returned from
-     * {@code release(Source<T>)}.
+     * {@code release(EventStream<T>)}.
      */
     public static final class Release<T> {
-        private final Source<T> input;
-        Release(Source<T> input) {
+        private final EventStream<T> input;
+        Release(EventStream<T> input) {
             this.input = input;
         }
-        public Source<T> on(Source<?> impulse) {
+        public EventStream<T> on(EventStream<?> impulse) {
             return releaseOnImpulse(input, impulse);
         }
     }
 
     /**
      * Type returned from
-     * {@code combine(Source<A>)}.
+     * {@code combine(EventStream<A>)}.
      */
     public static final class Combine1<A> {
-        private final Source<A> srcA;
-        Combine1(Source<A> srcA) {
+        private final EventStream<A> srcA;
+        Combine1(EventStream<A> srcA) {
             this.srcA = srcA;
         }
-        public <I> Combine1On<A, I> on(Source<I> impulse) {
+        public <I> Combine1On<A, I> on(EventStream<I> impulse) {
             return new Combine1On<A, I>(srcA, impulse);
         }
     }
 
     /**
      * Type returned from
-     * {@code combine(Source<A>).on(Source<I>)}.
+     * {@code combine(EventStream<A>).on(EventStream<I>)}.
      */
     public static final class Combine1On<A, I> {
-        private final Source<A> srcA;
-        private final Source<I> impulse;
-        Combine1On(Source<A> srcA, Source<I> impulse) {
+        private final EventStream<A> srcA;
+        private final EventStream<I> impulse;
+        Combine1On(EventStream<A> srcA, EventStream<I> impulse) {
             this.srcA = srcA;
             this.impulse = impulse;
         }
-        public <R> Source<R> by(Combinator2<A, I, R> combinator) {
+        public <R> EventStream<R> by(Combinator2<A, I, R> combinator) {
             return combineOnImpulse(srcA, impulse, combinator);
         }
     }
 
     /**
      * Type returned from
-     * {@code combine(Source<A>, Source<B>)}.
+     * {@code combine(EventStream<A>, EventStream<B>)}.
      */
     public static final class Combine2<A, B> {
-        private final Source<A> srcA;
-        private final Source<B> srcB;
-        Combine2(Source<A> srcA, Source<B> srcB) {
+        private final EventStream<A> srcA;
+        private final EventStream<B> srcB;
+        Combine2(EventStream<A> srcA, EventStream<B> srcB) {
             this.srcA = srcA;
             this.srcB = srcB;
         }
-        public <R> Source<R> by(Combinator2<A, B, R> combinator) {
+        public <R> EventStream<R> by(Combinator2<A, B, R> combinator) {
             return combineLatest(srcA, srcB, combinator);
         }
-        public <I> Combine2On<A, B, I> on(Source<I> impulse) {
+        public <I> Combine2On<A, B, I> on(EventStream<I> impulse) {
             return new Combine2On<A, B, I>(srcA, srcB, impulse);
         }
     }
 
     /**
      * Type returned from
-     * {@code combine(Source<A>, Source<B>).on(Source<I>)}.
+     * {@code combine(EventStream<A>, EventStream<B>).on(EventStream<I>)}.
      */
     public static final class Combine2On<A, B, I> {
-        private final Source<A> srcA;
-        private final Source<B> srcB;
-        private final Source<I> impulse;
-        Combine2On(Source<A> srcA, Source<B> srcB, Source<I> impulse) {
+        private final EventStream<A> srcA;
+        private final EventStream<B> srcB;
+        private final EventStream<I> impulse;
+        Combine2On(EventStream<A> srcA, EventStream<B> srcB, EventStream<I> impulse) {
             this.srcA = srcA;
             this.srcB = srcB;
             this.impulse = impulse;
         }
-        public <R> Source<R> by(Combinator3<A, B, I, R> combinator) {
+        public <R> EventStream<R> by(Combinator3<A, B, I, R> combinator) {
             return combineOnImpulse(srcA, srcB, impulse, combinator);
         }
     }
 
     /**
      * Type returned from
-     * {@code combine(Source<A>, Source<B>, Source<C>)}.
+     * {@code combine(EventStream<A>, EventStream<B>, EventStream<C>)}.
      */
     public static final class Combine3<A, B, C> {
-        private final Source<A> srcA;
-        private final Source<B> srcB;
-        private final Source<C> srcC;
-        Combine3(Source<A> srcA, Source<B> srcB, Source<C> srcC) {
+        private final EventStream<A> srcA;
+        private final EventStream<B> srcB;
+        private final EventStream<C> srcC;
+        Combine3(EventStream<A> srcA, EventStream<B> srcB, EventStream<C> srcC) {
             this.srcA = srcA;
             this.srcB = srcB;
             this.srcC = srcC;
         }
-        public <R> Source<R> by(Combinator3<A, B, C, R> combinator) {
+        public <R> EventStream<R> by(Combinator3<A, B, C, R> combinator) {
             return combineLatest(srcA, srcB, srcC, combinator);
         }
-        public <I> Combine3On<A, B, C, I> on(Source<I> impulse) {
+        public <I> Combine3On<A, B, C, I> on(EventStream<I> impulse) {
             return new Combine3On<A, B, C, I>(srcA, srcB, srcC, impulse);
         }
     }
 
     /**
      * Type returned from
-     * {@code combine(Source<A>, Source<B>, Source<C>).on(Source<I>)}.
+     * {@code combine(EventStream<A>, EventStream<B>, EventStream<C>).on(EventStream<I>)}.
      */
     public static final class Combine3On<A, B, C, I> {
-        private final Source<A> srcA;
-        private final Source<B> srcB;
-        private final Source<C> srcC;
-        private final Source<I> impulse;
-        Combine3On(Source<A> srcA, Source<B> srcB, Source<C> srcC, Source<I> impulse) {
+        private final EventStream<A> srcA;
+        private final EventStream<B> srcB;
+        private final EventStream<C> srcC;
+        private final EventStream<I> impulse;
+        Combine3On(EventStream<A> srcA, EventStream<B> srcB, EventStream<C> srcC, EventStream<I> impulse) {
             this.srcA = srcA;
             this.srcB = srcB;
             this.srcC = srcC;
             this.impulse = impulse;
         }
-        public <R> Source<R> by(Combinator4<A, B, C, I, R> combinator) {
+        public <R> EventStream<R> by(Combinator4<A, B, C, I, R> combinator) {
             return combineOnImpulse(srcA, srcB, srcC, impulse, combinator);
         }
     }
 
-    public static Source<Void> fromInvalidations(Observable observable) {
-        return new CombinedSource<Void>() {
+    public static EventStream<Void> invalidationsOf(Observable observable) {
+        return new CombinedStream<Void>() {
             private final InvalidationListener listener = obs -> emit(null);
             private final Subscription subscription = () -> observable.removeListener(listener);
 
@@ -159,8 +159,8 @@ public class Sources {
         };
     }
 
-    public static <T> Source<T> fromChanges(ObservableValue<T> observable) {
-        return new CombinedSource<T>() {
+    public static <T> EventStream<T> valuesOf(ObservableValue<T> observable) {
+        return new CombinedStream<T>() {
             private final ChangeListener<T> listener = (obs, old, val) -> emit(val);
             private final Subscription subscription = () -> observable.removeListener(listener);
 
@@ -172,8 +172,8 @@ public class Sources {
         };
     }
 
-    public static <T> Source<T> filter(Source<? extends T> input, Predicate<T> predicate) {
-        return new CombinedSource<T>() {
+    public static <T> EventStream<T> filter(EventStream<? extends T> input, Predicate<T> predicate) {
+        return new CombinedStream<T>() {
             @Override
             protected Subscription subscribeToInputs() {
                 return input.subscribe(value -> {
@@ -185,8 +185,8 @@ public class Sources {
         };
     }
 
-    public static <T, U> Source<U> map(Source<T> input, Function<T, U> f) {
-        return new CombinedSource<U>() {
+    public static <T, U> EventStream<U> map(EventStream<T> input, Function<T, U> f) {
+        return new CombinedStream<U>() {
             @Override
             protected Subscription subscribeToInputs() {
                 return input.subscribe(value -> {
@@ -197,8 +197,8 @@ public class Sources {
     }
 
     @SafeVarargs
-    public static <T> Source<T> merge(Source<? extends T>... inputs) {
-        return new CombinedSource<T>() {
+    public static <T> EventStream<T> merge(EventStream<? extends T>... inputs) {
+        return new CombinedStream<T>() {
             @Override
             protected Subscription subscribeToInputs() {
                 Subscription[] subs = new Subscription[inputs.length];
@@ -210,8 +210,8 @@ public class Sources {
         };
     }
 
-    public static <A, B, R> Source<R> zip(Source<A> srcA, Source<B> srcB, Combinator2<A, B, R> combinator) {
-        return new ZippedSource<R>() {
+    public static <A, B, R> EventStream<R> zip(EventStream<A> srcA, EventStream<B> srcB, Combinator2<A, B, R> combinator) {
+        return new ZippedStream<R>() {
             Pocket<A> pocketA = new Pocket<A>();
             Pocket<B> pocketB = new Pocket<B>();
 
@@ -231,8 +231,8 @@ public class Sources {
         };
     }
 
-    public static <A, B, C, R> Source<R> zip(Source<A> srcA, Source<B> srcB, Source<C> srcC, Combinator3<A, B, C, R> combinator) {
-        return new ZippedSource<R>() {
+    public static <A, B, C, R> EventStream<R> zip(EventStream<A> srcA, EventStream<B> srcB, EventStream<C> srcC, Combinator3<A, B, C, R> combinator) {
+        return new ZippedStream<R>() {
             Pocket<A> pocketA = new Pocket<A>();
             Pocket<B> pocketB = new Pocket<B>();
             Pocket<C> pocketC = new Pocket<C>();
@@ -254,8 +254,8 @@ public class Sources {
         };
     }
 
-    public static <A, I, R> Source<R> combineOnImpulse(Source<A> srcA, Source<I> impulse, Combinator2<A, I, R> combinator) {
-        return new CombinedSource<R>() {
+    public static <A, I, R> EventStream<R> combineOnImpulse(EventStream<A> srcA, EventStream<I> impulse, Combinator2<A, I, R> combinator) {
+        return new CombinedStream<R>() {
             Pocket<A> pocketA = new OverwritingPocket<A>();
 
             @Override
@@ -273,12 +273,12 @@ public class Sources {
         };
     }
 
-    public static <A> Combine1<A> combine(Source<A> srcA) {
+    public static <A> Combine1<A> combine(EventStream<A> srcA) {
         return new Combine1<A>(srcA);
     }
 
-    public static <A, B, R> Source<R> combineLatest(Source<A> srcA, Source<B> srcB, Combinator2<A, B, R> combinator) {
-        return new CombineLatestSource<R>() {
+    public static <A, B, R> EventStream<R> combineLatest(EventStream<A> srcA, EventStream<B> srcB, Combinator2<A, B, R> combinator) {
+        return new CombineLatestStream<R>() {
             Pocket<A> pocketA = new Pocket<A>();
             Pocket<B> pocketB = new Pocket<B>();
 
@@ -298,8 +298,8 @@ public class Sources {
         };
     }
 
-    public static <A, B, I, R> Source<R> combineOnImpulse(Source<A> srcA, Source<B> srcB, Source<I> impulse, Combinator3<A, B, I, R> combinator) {
-        return new CombinedSource<R>() {
+    public static <A, B, I, R> EventStream<R> combineOnImpulse(EventStream<A> srcA, EventStream<B> srcB, EventStream<I> impulse, Combinator3<A, B, I, R> combinator) {
+        return new CombinedStream<R>() {
             Pocket<A> pocketA = new OverwritingPocket<A>();
             Pocket<B> pocketB = new OverwritingPocket<B>();
 
@@ -319,12 +319,12 @@ public class Sources {
         };
     }
 
-    public static <A, B> Combine2<A, B> combine(Source<A> srcA, Source<B> srcB) {
+    public static <A, B> Combine2<A, B> combine(EventStream<A> srcA, EventStream<B> srcB) {
         return new Combine2<A, B>(srcA, srcB);
     }
 
-    public static <A, B, C, R> Source<R> combineLatest(Source<A> srcA, Source<B> srcB, Source<C> srcC, Combinator3<A, B, C, R> combinator) {
-        return new CombineLatestSource<R>() {
+    public static <A, B, C, R> EventStream<R> combineLatest(EventStream<A> srcA, EventStream<B> srcB, EventStream<C> srcC, Combinator3<A, B, C, R> combinator) {
+        return new CombineLatestStream<R>() {
             Pocket<A> pocketA = new Pocket<A>();
             Pocket<B> pocketB = new Pocket<B>();
             Pocket<C> pocketC = new Pocket<C>();
@@ -346,8 +346,8 @@ public class Sources {
         };
     }
 
-    public static <A, B, C, I, R> Source<R> combineOnImpulse(Source<A> srcA, Source<B> srcB, Source<C> srcC, Source<I> impulse, Combinator4<A, B, C, I, R> combinator) {
-        return new CombinedSource<R>() {
+    public static <A, B, C, I, R> EventStream<R> combineOnImpulse(EventStream<A> srcA, EventStream<B> srcB, EventStream<C> srcC, EventStream<I> impulse, Combinator4<A, B, C, I, R> combinator) {
+        return new CombinedStream<R>() {
             Pocket<A> pocketA = new OverwritingPocket<A>();
             Pocket<B> pocketB = new OverwritingPocket<B>();
             Pocket<C> pocketC = new OverwritingPocket<C>();
@@ -369,16 +369,16 @@ public class Sources {
         };
     }
 
-    public static <A, B, C> Combine3<A, B, C> combine(Source<A> srcA, Source<B> srcB, Source<C> srcC) {
+    public static <A, B, C> Combine3<A, B, C> combine(EventStream<A> srcA, EventStream<B> srcB, EventStream<C> srcC) {
         return new Combine3<A, B, C>(srcA, srcB, srcC);
     }
 
-    public static <T> Release<T> release(Source<T> input) {
+    public static <T> Release<T> release(EventStream<T> input) {
         return new Release<T>(input);
     }
 
-    public static <T> Source<T> releaseOnImpulse(Source<T> input, Source<?> impulse) {
-        return new CombinedSource<T>() {
+    public static <T> EventStream<T> releaseOnImpulse(EventStream<T> input, EventStream<?> impulse) {
+        return new CombinedStream<T>() {
             private boolean hasValue = false;
             private T value = null;
 
@@ -403,7 +403,7 @@ public class Sources {
     }
 
 
-    private static abstract class CombinedSource<T> extends SourceBase<T> {
+    private static abstract class CombinedStream<T> extends EventStreamBase<T> {
         private Subscription subscription = null;
 
         protected abstract Subscription subscribeToInputs();
@@ -420,7 +420,7 @@ public class Sources {
         }
     }
 
-    private static abstract class ZippedSource<T> extends CombinedSource<T> {
+    private static abstract class ZippedStream<T> extends CombinedStream<T> {
 
         class Pocket<A> extends ExclusivePocket<A> {
             @Override
@@ -432,7 +432,7 @@ public class Sources {
         abstract void tryEmit();
     }
 
-    private static abstract class CombineLatestSource<T> extends CombinedSource<T> {
+    private static abstract class CombineLatestStream<T> extends CombinedStream<T> {
 
         class Pocket<A> extends OverwritingPocket<A> {
             @Override
@@ -464,7 +464,7 @@ public class Sources {
             return res;
         }
 
-        public Subscription fillFrom(Source<T> src) {
+        public Subscription fillFrom(EventStream<T> src) {
             hasValue = false;
             value = null;
             return src.subscribe(a -> push(a));

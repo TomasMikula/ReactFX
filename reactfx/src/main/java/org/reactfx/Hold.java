f1e557127cab71dd535882d07803037a5bad9417
Tomas Mikula <tomas.mikula@gmail.com>
2014-03-13 21:15:27 +0100

    Close multi-hold in reverse order.

--- reactfx/src/main/java/org/reactfx/Hold.java
+++ reactfx/src/main/java/org/reactfx/Hold.java
@@ -10,10 +10,17 @@ public interface Hold extends AutoCloseable {
     @Override
     void close();
 
+    /**
+     * Returns a hold that is a composition of multiple holds.
+     * Its {@code close()} method closes the holds in reverse order.
+     * @param holds holds that should be released (in reverse order)
+     * when the returned hold is released.
+     * @return
+     */
     static Hold multi(Hold... holds) {
         return () -> {
-            for(Hold h: holds) {
-                h.close();
+            for(int i = holds.length - 1; i >= 0; --i) {
+                holds[i].close();
             }
         };
     }

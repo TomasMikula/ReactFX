64f10f429f5579792bb380d79644fc67a06a1edc
Tomas Mikula <tomas.mikula@gmail.com>
2014-03-27 18:32:24 +0100

    Add specialized implementation for bi-subscription

--- reactfx/src/main/java/org/reactfx/Subscription.java
+++ reactfx/src/main/java/org/reactfx/Subscription.java
@@ -1,15 +1,48 @@
 package org.reactfx;
 
+@FunctionalInterface
 public interface Subscription {
     void unsubscribe();
 
     static final Subscription EMPTY = () -> {};
 
-    static Subscription multi(Subscription... subscriptions) {
-        return () -> {
-            for(Subscription s: subscriptions) {
-                s.unsubscribe();
-            }
-        };
+    static Subscription multi(Subscription... subs) {
+        switch(subs.length) {
+            case 0: return EMPTY;
+            case 1: return subs[0];
+            case 2: return new BiSubscription(subs[0], subs[1]);
+            default: return new MultiSubscription(subs);
+        }
+    }
+}
+
+class BiSubscription implements Subscription {
+    private final Subscription s1;
+    private final Subscription s2;
+
+    public BiSubscription(Subscription s1, Subscription s2) {
+        this.s1 = s1;
+        this.s2 = s2;
+    }
+
+    @Override
+    public void unsubscribe() {
+        s1.unsubscribe();
+        s2.unsubscribe();
+    }
+}
+
+class MultiSubscription implements Subscription {
+    private final Subscription[] subscriptions;
+
+    public MultiSubscription(Subscription... subscriptions) {
+        this.subscriptions = subscriptions;
+    }
+
+    @Override
+    public void unsubscribe() {
+        for(Subscription s: subscriptions) {
+            s.unsubscribe();
+        }
     }
 }

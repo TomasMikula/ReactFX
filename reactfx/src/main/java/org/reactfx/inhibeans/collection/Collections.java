d745de64c58fa0f557a85fea6799b1a3d36d0376
Tomas Mikula <tomas.mikula@gmail.com>
2014-05-13 12:03:16 +0200

    Make Collections final and non-instantiable.

--- reactfx/src/main/java/org/reactfx/inhibeans/collection/Collections.java
+++ reactfx/src/main/java/org/reactfx/inhibeans/collection/Collections.java
@@ -1,7 +1,16 @@
 package org.reactfx.inhibeans.collection;
 
-public class Collections {
+public final class Collections {
 
+    // private constructor to prevent instantiation
+    private Collections() {}
+
+    /**
+     * Creates an ObservableList wrapper that is able to temporarily block
+     * list change notifications.
+     * @param delegate the underlying observable list.
+     * @return
+     */
     public static <E> ObservableList<E> wrap(javafx.collections.ObservableList<E> delegate) {
         return new ObservableListWrapper<>(delegate);
     }

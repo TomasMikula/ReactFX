d4d707e323aedd305fc46e36fd48de0a6b4a5911
Tomas Mikula <tomas.mikula@gmail.com>
2014-12-04 17:28:30 +0100

    Move subscribeTo* helpers from EventStreamBase to LazilyBoundStreamBase.

--- reactfx/src/main/java/org/reactfx/EventStreamBase.java
+++ reactfx/src/main/java/org/reactfx/EventStreamBase.java
@@ -1,10 +1,8 @@
 package org.reactfx;
 
-import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 import org.reactfx.util.ListHelper;
-import org.reactfx.util.TriConsumer;
 
 /**
  *
@@ -109,45 +107,6 @@ public abstract class EventStreamBase<S> {
      */
     protected void noSubscribers() {
         // default implementation is empty
-    }
-
-    /**
-     * Subscribes to the given event stream by the given subscriber and also
-     * forwards errors reported by the given stream to this stream. This is
-     * equivalent to {@code stream.subscribe(subscriber, this::reportError)}.
-     * @return subscription used to unsubscribe {@code subscriber} from
-     * {@code stream} and stop forwarding the errors.
-     */
-    protected final <T> Subscription subscribeTo(
-            EventStream<T> stream,
-            Consumer<? super T> subscriber) {
-        return stream.subscribe(subscriber, this::reportError);
-    }
-
-    /**
-     * Subscribes to the given event stream by the given subscriber and also
-     * forwards errors reported by the given stream to this stream. This is
-     * equivalent to {@code stream.subscribe(subscriber, this::reportError)}.
-     * @return subscription used to unsubscribe {@code subscriber} from
-     * {@code stream} and stop forwarding the errors.
-     */
-    protected final <A, B> Subscription subscribeToBi(
-            BiEventStream<A, B> stream,
-            BiConsumer<? super A, ? super B> subscriber) {
-        return stream.subscribe(subscriber, this::reportError);
-    }
-
-    /**
-     * Subscribes to the given event stream by the given subscriber and also
-     * forwards errors reported by the given stream to this stream. This is
-     * equivalent to {@code stream.subscribe(subscriber, this::reportError)}.
-     * @return subscription used to unsubscribe {@code subscriber} from
-     * {@code stream} and stop forwarding the errors.
-     */
-    protected final <A, B, C> Subscription subscribeToTri(
-            TriEventStream<A, B, C> stream,
-            TriConsumer<? super A, ? super B, ? super C> subscriber) {
-        return stream.subscribe(subscriber, this::reportError);
     }
 
     public final Subscription subscribe(
--- reactfx/src/main/java/org/reactfx/LazilyBoundStreamBase.java
+++ reactfx/src/main/java/org/reactfx/LazilyBoundStreamBase.java
@@ -1,5 +1,10 @@
 package org.reactfx;
 
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+import org.reactfx.util.TriConsumer;
+
 /**
  * Base class for an event stream that has one or more sources (most commonly
  * event streams, but not necessarily) to which it is subscribed only when it
@@ -33,5 +38,44 @@ public abstract class LazilyBoundStreamBase<S> extends EventStreamBase<S> {
 
     protected final boolean isBound() {
         return subscription != null;
+    }
+
+    /**
+     * Subscribes to the given event stream by the given subscriber and also
+     * forwards errors reported by the given stream to this stream. This is
+     * equivalent to {@code stream.subscribe(subscriber, this::reportError)}.
+     * @return subscription used to unsubscribe {@code subscriber} from
+     * {@code stream} and stop forwarding the errors.
+     */
+    protected final <T> Subscription subscribeTo(
+            EventStream<T> stream,
+            Consumer<? super T> subscriber) {
+        return stream.subscribe(subscriber, this::reportError);
+    }
+
+    /**
+     * Subscribes to the given event stream by the given subscriber and also
+     * forwards errors reported by the given stream to this stream. This is
+     * equivalent to {@code stream.subscribe(subscriber, this::reportError)}.
+     * @return subscription used to unsubscribe {@code subscriber} from
+     * {@code stream} and stop forwarding the errors.
+     */
+    protected final <A, B> Subscription subscribeToBi(
+            BiEventStream<A, B> stream,
+            BiConsumer<? super A, ? super B> subscriber) {
+        return stream.subscribe(subscriber, this::reportError);
+    }
+
+    /**
+     * Subscribes to the given event stream by the given subscriber and also
+     * forwards errors reported by the given stream to this stream. This is
+     * equivalent to {@code stream.subscribe(subscriber, this::reportError)}.
+     * @return subscription used to unsubscribe {@code subscriber} from
+     * {@code stream} and stop forwarding the errors.
+     */
+    protected final <A, B, C> Subscription subscribeToTri(
+            TriEventStream<A, B, C> stream,
+            TriConsumer<? super A, ? super B, ? super C> subscriber) {
+        return stream.subscribe(subscriber, this::reportError);
     }
 }

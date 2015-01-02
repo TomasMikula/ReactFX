08a6659a95b98696ee7f1b3db6d4e2cd55b11959
Tomas Mikula <tomas.mikula@gmail.com>
2015-01-02 23:26:13 +0100

    Add index checking methods to Lists helper class.

--- reactfx/src/main/java/org/reactfx/util/Lists.java
+++ reactfx/src/main/java/org/reactfx/util/Lists.java
@@ -56,6 +56,86 @@ public final class Lists {
         return res.toString();
     }
 
+    public static boolean isValidIndex(int index, int size) {
+        return isValidIndex(0, index, size);
+    }
+
+    public static boolean isValidIndex(int min, int index, int max) {
+        return min <= index && index < max;
+    }
+
+    public static void checkIndex(int index, int size) {
+        checkIndex(0, index, size);
+    }
+
+    public static void checkIndex(int min, int index, int max) {
+        if(!isValidIndex(min, index, max)) {
+            throw new IndexOutOfBoundsException(index + " not in [" + min + ", " + max + ")");
+        }
+    }
+
+    public static boolean isValidPosition(int position, int size) {
+        return isValidPosition(0, position, size);
+    }
+
+    public static boolean isValidPosition(int min, int position, int max) {
+        return min <= position && position <= max;
+    }
+
+    public static void checkPosition(int position, int size) {
+        checkPosition(0, position, size);
+    }
+
+    public static void checkPosition(int min, int position, int max) {
+        if(!isValidPosition(min, position, max)) {
+            throw new IndexOutOfBoundsException(position + " not in [" + min + ", " + max + "]");
+        }
+    }
+
+    public static boolean isValidRange(int from, int to, int size) {
+        return isValidRange(0, from, to, size);
+    }
+
+    public static boolean isValidRange(int min, int from, int to, int max) {
+        return min <= from && from <= to && to <= max;
+    }
+
+    public static void checkRange(int from, int to, int size) {
+        checkRange(0, from, to, size);
+    }
+
+    public static void checkRange(int min, int from, int to, int max) {
+        if(!isValidRange(min, from, to, max)) {
+            throw new IndexOutOfBoundsException(
+                    "[" + from + ", " + to + ") is not a valid range within " +
+                    "[" + min + ", " + max + ")");
+        }
+    }
+
+    public static boolean isNonEmptyRange(int from, int to, int size) {
+        return isNonEmptyRange(0, from, to, size);
+    }
+
+    public static boolean isNonEmptyRange(int min, int from, int to, int max) {
+        return min <= from && from < to && to <= max;
+    }
+
+    public static boolean isProperRange(int from, int to, int size) {
+        return isProperRange(0, from, to, size);
+    }
+
+    public static boolean isProperRange(int min, int from, int to, int max) {
+        return isValidRange(min, from, to, max) && (min < from || to < max);
+    }
+
+    public static boolean isProperNonEmptyRange(int from, int to, int size) {
+        return isProperNonEmptyRange(0, from, to, size);
+    }
+
+    public static boolean isProperNonEmptyRange(int min, int from, int to, int max) {
+        return isNonEmptyRange(min, from, to, max) && (min < from || to < max);
+    }
+
     public static <E, F> List<F> mappedView(
             List<? extends E> source,
             Function<? super E, ? extends F> f) {

9131a929864dfb7db561f39130f753ea08e9da81
Tomas Mikula <tomas.mikula@gmail.com>
2015-02-20 18:09:17 -0500

    Add @Experimental annotation.

---
+++ reactfx/src/main/java/org/reactfx/util/Experimental.java
@@ -0,0 +1,23 @@
+package org.reactfx.util;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Used to annotate functionality that is likely to change or be removed in the
+ * future.
+ */
+@Documented
+@Retention(RetentionPolicy.CLASS)
+@Target({
+        ElementType.ANNOTATION_TYPE,
+        ElementType.CONSTRUCTOR,
+        ElementType.FIELD,
+        ElementType.METHOD,
+        ElementType.PACKAGE,
+        ElementType.TYPE
+})
+public @interface Experimental {}

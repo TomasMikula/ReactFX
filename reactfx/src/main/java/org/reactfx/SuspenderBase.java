5690f3354ce0c5f3688ac82cd90e2280e968cc05
Tomas Mikula <tomas.mikula@gmail.com>
2015-02-20 02:01:15 -0500

    Factor SuspenderBase out of SuspenderStreamImpl.

---
+++ reactfx/src/main/java/org/reactfx/SuspenderBase.java
@@ -0,0 +1,26 @@
+package org.reactfx;
+
+public abstract class SuspenderBase<O, T, S extends Suspendable>
+extends ObservableBase<O, T> implements Suspender<S> {
+    private final S suspendable;
+
+    protected SuspenderBase(S suspendable) {
+        this.suspendable = suspendable;
+    }
+
+    /**
+     * Use in subclasses instead of {@link #notifyObservers(Object)} in order
+     * to suspend the associated {@linkplain Suspendable} while notifying
+     * observers.
+     */
+    protected final void notifyObserversWhileSuspended(T notification) {
+        try(Guard g = suspendable.suspend()) {
+            notifyObservers(notification);
+        }
+    }
+
+    @Override
+    public final S getSuspendable() {
+        return suspendable;
+    }
+}
--- reactfx/src/main/java/org/reactfx/SuspenderStreamImpl.java
+++ reactfx/src/main/java/org/reactfx/SuspenderStreamImpl.java
@@ -1,26 +1,19 @@
 package org.reactfx;
 
+import java.util.function.Consumer;
+
 class SuspenderStreamImpl<T, S extends Suspendable>
-extends EventStreamBase<T> implements Suspender<S>, SuspenderStream<T, S> {
+extends SuspenderBase<Consumer<? super T>, T, S>
+implements SuspenderStream<T, S>, ProperEventStream<T> {
     private final EventStream<T> source;
-    private final S suspendable;
 
     public SuspenderStreamImpl(EventStream<T> source, S suspendable) {
+        super(suspendable);
         this.source = source;
-        this.suspendable = suspendable;
     }
 
     @Override
     protected Subscription observeInputs() {
-        return source.subscribe(evt -> {
-            try(Guard g = suspendable.suspend()) {
-                emit(evt);
-            }
-        });
-    }
-
-    @Override
-    public S getSuspendable() {
-        return suspendable;
+        return source.subscribe(this::notifyObserversWhileSuspended);
     }
 }

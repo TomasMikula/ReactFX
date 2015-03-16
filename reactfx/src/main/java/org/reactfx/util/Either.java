a077e849433c4255f525bc5c8fa9c66fb4398832
Tomas Mikula <tomas.mikula@gmail.com>
2015-03-16 00:29:24 -0400

    Make Either and Try interfaces, Try extends Either

--- reactfx/src/main/java/org/reactfx/util/Either.java
+++ reactfx/src/main/java/org/reactfx/util/Either.java
@@ -6,283 +6,282 @@ import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
-public abstract class Either<L, R> {
+public interface Either<L, R> {
 
-    private static class Left<L, R> extends Either<L, R> {
-        private final L value;
-
-        public Left(L value) { this.value = value; }
-
-        @Override
-        public boolean isLeft() { return true; }
-
-        @Override
-        public boolean isRight() { return false; }
-
-        @Override
-        public L getLeft() { return value; }
-
-        @Override
-        public R getRight() { throw new NoSuchElementException(); }
-
-        @Override
-        public L toLeft(Function<? super R, ? extends L> f) {
-            return value;
-        }
-
-        @Override
-        public R toRight(Function<? super L, ? extends R> f) {
-            return f.apply(value);
-        }
-
-        @Override
-        public Optional<L> asLeft() { return Optional.of(value); }
-
-        @Override
-        public Optional<R> asRight() { return Optional.empty(); }
-
-        @Override
-        public void ifLeft(Consumer<? super L> f) { f.accept(value); }
-
-        @Override
-        public void ifRight(Consumer<? super R> f) { /* do nothing */ }
-
-        @Override
-        public void exec(
-                Consumer<? super L> ifLeft,
-                Consumer<? super R> ifRight) {
-            ifLeft.accept(value);
-        }
-
-        @Override
-        public <L2> Either<L2, R> mapLeft(Function<? super L, ? extends L2> f) {
-            return left(f.apply(value));
-        }
-
-        @Override
-        public <R2> Either<L, R2> mapRight(Function<? super R, ? extends R2> f) {
-            return left(value);
-        }
-
-        @Override
-        public <L2, R2> Either<L2, R2> map(
-                Function<? super L, ? extends L2> f,
-                Function<? super R, ? extends R2> g) {
-            return left(f.apply(value));
-        }
-
-        @Override
-        public <L2, R2> Either<L2, R2> flatMap(
-                Function<? super L, Either<L2, R2>> f,
-                Function<? super R, Either<L2, R2>> g) {
-            return f.apply(value);
-        }
-
-        @Override
-        public <L2> Either<L2, R> flatMapLeft(
-                Function<? super L, Either<L2, R>> f) {
-            return f.apply(value);
-        }
-
-        @Override
-        public <R2> Either<L, R2> flatMapRight(
-                Function<? super R, Either<L, R2>> f) {
-            return left(value);
-        }
-
-        @Override
-        public <T> T unify(
-                Function<? super L, ? extends T> f,
-                Function<? super R, ? extends T> g) {
-            return f.apply(value);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(value);
-        }
-
-        @Override
-        public boolean equals(Object other) {
-            if(other instanceof Left) {
-                Left<?, ?> that = (Left<?, ?>) other;
-                return Objects.equals(this.value, that.value);
-            } else {
-                return false;
-            }
-        }
-
-        @Override
-        public String toString() {
-            return "left(" + value + ")";
-        }
-    }
-
-    private static class Right<L, R> extends Either<L, R> {
-        private final R value;
-
-        public Right(R value) { this.value = value; }
-
-        @Override
-        public boolean isLeft() { return false; }
-
-        @Override
-        public boolean isRight() { return true; }
-
-        @Override
-        public L getLeft() { throw new NoSuchElementException(); }
-
-        @Override
-        public R getRight() { return value; }
-
-        @Override
-        public L toLeft(Function<? super R, ? extends L> f) {
-            return f.apply(value);
-        }
-
-        @Override
-        public R toRight(Function<? super L, ? extends R> f) {
-            return value;
-        }
-
-        @Override
-        public Optional<L> asLeft() { return Optional.empty(); }
-
-        @Override
-        public Optional<R> asRight() { return Optional.of(value); }
-
-        @Override
-        public void ifLeft(Consumer<? super L> f) { /* do nothing */ }
-
-        @Override
-        public void ifRight(Consumer<? super R> f) { f.accept(value); }
-
-        @Override
-        public void exec(
-                Consumer<? super L> ifLeft,
-                Consumer<? super R> ifRight) {
-            ifRight.accept(value);
-        }
-
-        @Override
-        public <L2> Either<L2, R> mapLeft(Function<? super L, ? extends L2> f) {
-            return right(value);
-        }
-
-        @Override
-        public <R2> Either<L, R2> mapRight(Function<? super R, ? extends R2> f) {
-            return right(f.apply(value));
-        }
-
-        @Override
-        public <L2, R2> Either<L2, R2> map(
-                Function<? super L, ? extends L2> f,
-                Function<? super R, ? extends R2> g) {
-            return right(g.apply(value));
-        }
-
-        @Override
-        public <L2, R2> Either<L2, R2> flatMap(
-                Function<? super L, Either<L2, R2>> f,
-                Function<? super R, Either<L2, R2>> g) {
-            return g.apply(value);
-        }
-
-        @Override
-        public <L2> Either<L2, R> flatMapLeft(
-                Function<? super L, Either<L2, R>> f) {
-            return right(value);
-        }
-
-        @Override
-        public <R2> Either<L, R2> flatMapRight(
-                Function<? super R, Either<L, R2>> f) {
-            return f.apply(value);
-        }
-
-        @Override
-        public <T> T unify(
-                Function<? super L, ? extends T> f,
-                Function<? super R, ? extends T> g) {
-            return g.apply(value);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(value);
-        }
-
-        @Override
-        public boolean equals(Object other) {
-            if(other instanceof Right) {
-                Right<?, ?> that = (Right<?, ?>) other;
-                return Objects.equals(this.value, that.value);
-            } else {
-                return false;
-            }
-        }
-
-        @Override
-        public String toString() {
-            return "right(" + value + ")";
-        }
-    }
-
-    public static <L, R> Either<L, R> left(L l) {
+    static <L, R> Either<L, R> left(L l) {
         return new Left<>(l);
     }
 
-    public static <L, R> Either<L, R> right(R r) {
+    static <L, R> Either<L, R> right(R r) {
         return new Right<>(r);
     }
 
-    public static <L, R> Either<L, R> leftOrNull(Optional<L> l) {
+    static <L, R> Either<L, R> leftOrNull(Optional<L> l) {
         return leftOrDefault(l, null);
     }
 
-    public static <L, R> Either<L, R> rightOrNull(Optional<R> r) {
+    static <L, R> Either<L, R> rightOrNull(Optional<R> r) {
         return rightOrDefault(r, null);
     }
 
-    public static <L, R> Either<L, R> leftOrDefault(Optional<L> l, R r) {
+    static <L, R> Either<L, R> leftOrDefault(Optional<L> l, R r) {
         return l.isPresent() ? left(l.get()) : right(r);
     }
 
-    public static <L, R> Either<L, R> rightOrDefault(Optional<R> r, L l) {
+    static <L, R> Either<L, R> rightOrDefault(Optional<R> r, L l) {
         return r.isPresent() ? right(r.get()) : left(l);
     }
 
-    // private constructor to prevent subclassing
-    private Either() {}
-
-    public abstract boolean isLeft();
-    public abstract boolean isRight();
-    public abstract L getLeft();
-    public abstract R getRight();
-    public abstract L toLeft(Function<? super R, ? extends L> f);
-    public abstract R toRight(Function<? super L, ? extends R> f);
-    public abstract Optional<L> asLeft();
-    public abstract Optional<R> asRight();
-    public abstract void ifLeft(Consumer<? super L> f);
-    public abstract void ifRight(Consumer<? super R> f);
-    public abstract void exec(
+    boolean isLeft();
+    boolean isRight();
+    L getLeft();
+    R getRight();
+    L toLeft(Function<? super R, ? extends L> f);
+    R toRight(Function<? super L, ? extends R> f);
+    Optional<L> asLeft();
+    Optional<R> asRight();
+    void ifLeft(Consumer<? super L> f);
+    void ifRight(Consumer<? super R> f);
+    void exec(
             Consumer<? super L> ifLeft,
             Consumer<? super R> ifRight);
-    public abstract <L2> Either<L2, R> mapLeft(
+    <L2> Either<L2, R> mapLeft(
             Function<? super L, ? extends L2> f);
-    public abstract <R2> Either<L, R2> mapRight(
+    <R2> Either<L, R2> mapRight(
             Function<? super R, ? extends R2> f);
-    public abstract <L2, R2> Either<L2, R2> map(
+    <L2, R2> Either<L2, R2> map(
             Function<? super L, ? extends L2> f,
             Function<? super R, ? extends R2> g);
-    public abstract <L2, R2> Either<L2, R2> flatMap(
+    <L2, R2> Either<L2, R2> flatMap(
             Function<? super L, Either<L2, R2>> f,
             Function<? super R, Either<L2, R2>> g);
-    public abstract <L2> Either<L2, R> flatMapLeft(
+    <L2> Either<L2, R> flatMapLeft(
             Function<? super L, Either<L2, R>> f);
-    public abstract <R2> Either<L, R2> flatMapRight(
+    <R2> Either<L, R2> flatMapRight(
             Function<? super R, Either<L, R2>> f);
-    public abstract <T> T unify(
+    <T> T unify(
             Function<? super L, ? extends T> f,
             Function<? super R, ? extends T> g);
+}
+
+
+class Left<L, R> implements Either<L, R> {
+    private final L value;
+
+    public Left(L value) { this.value = value; }
+
+    @Override
+    public boolean isLeft() { return true; }
+
+    @Override
+    public boolean isRight() { return false; }
+
+    @Override
+    public L getLeft() { return value; }
+
+    @Override
+    public R getRight() { throw new NoSuchElementException(); }
+
+    @Override
+    public L toLeft(Function<? super R, ? extends L> f) {
+        return value;
+    }
+
+    @Override
+    public R toRight(Function<? super L, ? extends R> f) {
+        return f.apply(value);
+    }
+
+    @Override
+    public Optional<L> asLeft() { return Optional.of(value); }
+
+    @Override
+    public Optional<R> asRight() { return Optional.empty(); }
+
+    @Override
+    public void ifLeft(Consumer<? super L> f) { f.accept(value); }
+
+    @Override
+    public void ifRight(Consumer<? super R> f) { /* do nothing */ }
+
+    @Override
+    public void exec(
+            Consumer<? super L> ifLeft,
+            Consumer<? super R> ifRight) {
+        ifLeft.accept(value);
+    }
+
+    @Override
+    public <L2> Either<L2, R> mapLeft(Function<? super L, ? extends L2> f) {
+        return new Left<>(f.apply(value));
+    }
+
+    @Override
+    public <R2> Either<L, R2> mapRight(Function<? super R, ? extends R2> f) {
+        return new Left<>(value);
+    }
+
+    @Override
+    public <L2, R2> Either<L2, R2> map(
+            Function<? super L, ? extends L2> f,
+            Function<? super R, ? extends R2> g) {
+        return new Left<>(f.apply(value));
+    }
+
+    @Override
+    public <L2, R2> Either<L2, R2> flatMap(
+            Function<? super L, Either<L2, R2>> f,
+            Function<? super R, Either<L2, R2>> g) {
+        return f.apply(value);
+    }
+
+    @Override
+    public <L2> Either<L2, R> flatMapLeft(
+            Function<? super L, Either<L2, R>> f) {
+        return f.apply(value);
+    }
+
+    @Override
+    public <R2> Either<L, R2> flatMapRight(
+            Function<? super R, Either<L, R2>> f) {
+        return new Left<>(value);
+    }
+
+    @Override
+    public <T> T unify(
+            Function<? super L, ? extends T> f,
+            Function<? super R, ? extends T> g) {
+        return f.apply(value);
+    }
+
+    @Override
+    public final int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public final boolean equals(Object other) {
+        if(other instanceof Left) {
+            Left<?, ?> that = (Left<?, ?>) other;
+            return Objects.equals(this.value, that.value);
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "left(" + value + ")";
+    }
+}
+
+
+class Right<L, R> implements Either<L, R> {
+    private final R value;
+
+    public Right(R value) { this.value = value; }
+
+    @Override
+    public boolean isLeft() { return false; }
+
+    @Override
+    public boolean isRight() { return true; }
+
+    @Override
+    public L getLeft() { throw new NoSuchElementException(); }
+
+    @Override
+    public R getRight() { return value; }
+
+    @Override
+    public L toLeft(Function<? super R, ? extends L> f) {
+        return f.apply(value);
+    }
+
+    @Override
+    public R toRight(Function<? super L, ? extends R> f) {
+        return value;
+    }
+
+    @Override
+    public Optional<L> asLeft() { return Optional.empty(); }
+
+    @Override
+    public Optional<R> asRight() { return Optional.of(value); }
+
+    @Override
+    public void ifLeft(Consumer<? super L> f) { /* do nothing */ }
+
+    @Override
+    public void ifRight(Consumer<? super R> f) { f.accept(value); }
+
+    @Override
+    public void exec(
+            Consumer<? super L> ifLeft,
+            Consumer<? super R> ifRight) {
+        ifRight.accept(value);
+    }
+
+    @Override
+    public <L2> Either<L2, R> mapLeft(Function<? super L, ? extends L2> f) {
+        return new Right<>(value);
+    }
+
+    @Override
+    public <R2> Either<L, R2> mapRight(Function<? super R, ? extends R2> f) {
+        return new Right<>(f.apply(value));
+    }
+
+    @Override
+    public <L2, R2> Either<L2, R2> map(
+            Function<? super L, ? extends L2> f,
+            Function<? super R, ? extends R2> g) {
+        return new Right<>(g.apply(value));
+    }
+
+    @Override
+    public <L2, R2> Either<L2, R2> flatMap(
+            Function<? super L, Either<L2, R2>> f,
+            Function<? super R, Either<L2, R2>> g) {
+        return g.apply(value);
+    }
+
+    @Override
+    public <L2> Either<L2, R> flatMapLeft(
+            Function<? super L, Either<L2, R>> f) {
+        return new Right<>(value);
+    }
+
+    @Override
+    public <R2> Either<L, R2> flatMapRight(
+            Function<? super R, Either<L, R2>> f) {
+        return f.apply(value);
+    }
+
+    @Override
+    public <T> T unify(
+            Function<? super L, ? extends T> f,
+            Function<? super R, ? extends T> g) {
+        return g.apply(value);
+    }
+
+    @Override
+    public final int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public final boolean equals(Object other) {
+        if(other instanceof Right) {
+            Right<?, ?> that = (Right<?, ?>) other;
+            return Objects.equals(this.value, that.value);
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "right(" + value + ")";
+    }
 }
--- reactfx/src/main/java/org/reactfx/util/Try.java
+++ reactfx/src/main/java/org/reactfx/util/Try.java
@@ -1,181 +1,22 @@
 package org.reactfx.util;
 
-import java.util.NoSuchElementException;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.Callable;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
-public abstract class Try<T> {
+public interface Try<T> extends Either<Throwable, T> {
 
-    private static class Success<T> extends Try<T> {
-        private final T value;
-
-        public Success(T value) { this.value = value; }
-
-        @Override
-        public boolean isSuccess() { return true; }
-
-        @Override
-        public boolean isFailure() { return false; }
-
-        @Override
-        public T get() { return value; }
-
-        @Override
-        public T getOrElse(T fallback) { return value; }
-
-        @Override
-        public T getOrElse(Supplier<T> fallback) { return value; }
-
-        @Override
-        public Throwable getFailure() { throw new NoSuchElementException(); }
-
-        @Override
-        public Try<T> orElse(Try<T> fallback) { return this; }
-
-        @Override
-        public Try<T> orElse(Supplier<Try<T>> fallback) { return this; }
-
-        @Override
-        public Try<T> orElseTry(Callable<? extends T> fallback) { return this; }
-
-        @Override
-        public Try<T> recover(Function<Throwable, Optional<T>> f) { return this; }
-
-        @Override
-        public Optional<T> toOptional() { return Optional.of(value); }
-
-        @Override
-        public void ifSuccess(Consumer<? super T> f) { f.accept(value); }
-
-        @Override
-        public void ifFailure(Consumer<? super Throwable> f) { /* do nothing */ }
-
-        @Override
-        public <U> Try<U> map(Function<? super T, ? extends U> f) {
-            return success(f.apply(value));
-        }
-
-        @Override
-        public <U> Try<U> flatMap(Function<? super T, Try<U>> f) {
-            return f.apply(value);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(value);
-        }
-
-        @Override
-        public boolean equals(Object other) {
-            if(other instanceof Success) {
-                Success<?> that = (Success<?>) other;
-                return Objects.equals(this.value, that.value);
-            } else {
-                return false;
-            }
-        }
-
-        @Override
-        public String toString() {
-            return "success(" + value + ")";
-        }
-    }
-
-    private static class Failure<T> extends Try<T> {
-        private final Throwable thrown;
-
-        public Failure(Throwable thrown) { this.thrown = thrown; }
-
-        @Override
-        public boolean isSuccess() { return false; }
-
-        @Override
-        public boolean isFailure() { return true; }
-
-        @Override
-        public T get() { throw new NoSuchElementException(); }
-
-        @Override
-        public T getOrElse(T fallback) { return fallback; }
-
-        @Override
-        public T getOrElse(Supplier<T> fallback) { return fallback.get(); }
-
-        @Override
-        public Throwable getFailure() { return thrown; }
-
-        @Override
-        public Try<T> orElse(Try<T> fallback) { return fallback; }
-
-        @Override
-        public Try<T> orElse(Supplier<Try<T>> fallback) { return fallback.get(); }
-
-        @Override
-        public Try<T> orElseTry(Callable<? extends T> fallback) { return tryGet(fallback); }
-
-        @Override
-        public Try<T> recover(Function<Throwable, Optional<T>> f) {
-            Optional<T> recovered = f.apply(thrown);
-            if(recovered.isPresent()) {
-                return success(recovered.get());
-            } else {
-                return this;
-            }
-        }
-
-        @Override
-        public Optional<T> toOptional() { return Optional.empty(); }
-
-        @Override
-        public void ifSuccess(Consumer<? super T> f) { /* do nothing */ }
-
-        @Override
-        public void ifFailure(Consumer<? super Throwable> f) { f.accept(thrown); }
-
-        @Override
-        public <U> Try<U> map(Function<? super T, ? extends U> f) {
-            return failure(thrown);
-        }
-
-        @Override
-        public <U> Try<U> flatMap(Function<? super T, Try<U>> f) {
-            return failure(thrown);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(thrown);
-        }
-
-        @Override
-        public boolean equals(Object other) {
-            if(other instanceof Failure) {
-                Failure<?> that = (Failure<?>) other;
-                return Objects.equals(this.thrown, that.thrown);
-            } else {
-                return false;
-            }
-        }
-
-        @Override
-        public String toString() {
-            return "failure(" + thrown + ")";
-        }
-    }
-
-    public static <T> Try<T> success(T value) {
+    static <T> Try<T> success(T value) {
         return new Success<>(value);
     }
 
-    public static <T> Try<T> failure(Throwable thrown) {
+    static <T> Try<T> failure(Throwable thrown) {
         return new Failure<>(thrown);
     }
 
-    public static <T> Try<T> tryGet(Callable<? extends T> f) {
+    static <T> Try<T> tryGet(Callable<? extends T> f) {
         try {
             return success(f.call());
         } catch(Throwable t) {
@@ -183,22 +24,103 @@ public abstract class Try<T> {
         }
     }
 
-    // private constructor to prevent subclassing
-    private Try() {}
+    default boolean isSuccess() { return isRight(); }
+    default boolean isFailure() { return isLeft(); }
+    default T get() { return getRight(); }
+    default Throwable getFailure() { return getLeft(); }
+    default Optional<T> toOptional() { return asRight(); }
+    default void ifSuccess(Consumer<? super T> f) { ifRight(f); }
+    default void ifFailure(Consumer<? super Throwable> f) { ifLeft(f); }
 
-    public abstract boolean isSuccess();
-    public abstract boolean isFailure();
-    public abstract T get();
-    public abstract T getOrElse(T fallback);
-    public abstract T getOrElse(Supplier<T> fallback);
-    public abstract Throwable getFailure();
-    public abstract Try<T> orElse(Try<T> fallback);
-    public abstract Try<T> orElse(Supplier<Try<T>> fallback);
-    public abstract Try<T> orElseTry(Callable<? extends T> fallback);
-    public abstract Try<T> recover(Function<Throwable, Optional<T>> f);
-    public abstract Optional<T> toOptional();
-    public abstract void ifSuccess(Consumer<? super T> f);
-    public abstract void ifFailure(Consumer<? super Throwable> f);
-    public abstract <U> Try<U> map(Function<? super T, ? extends U> f);
-    public abstract <U> Try<U> flatMap(Function<? super T, Try<U>> f);
+    T getOrElse(T fallback);
+    T getOrElse(Supplier<T> fallback);
+    Try<T> orElse(Try<T> fallback);
+    Try<T> orElse(Supplier<Try<T>> fallback);
+    Try<T> orElseTry(Callable<? extends T> fallback);
+    Try<T> recover(Function<Throwable, Optional<T>> f);
+    <U> Try<U> map(Function<? super T, ? extends U> f);
+    <U> Try<U> flatMap(Function<? super T, Try<U>> f);
+}
+
+class Success<T> extends Right<Throwable, T> implements Try<T> {
+
+    public Success(T value) { super(value); }
+
+    @Override
+    public T getOrElse(T fallback) { return getRight(); }
+
+    @Override
+    public T getOrElse(Supplier<T> fallback) { return getRight(); }
+
+    @Override
+    public Try<T> orElse(Try<T> fallback) { return this; }
+
+    @Override
+    public Try<T> orElse(Supplier<Try<T>> fallback) { return this; }
+
+    @Override
+    public Try<T> orElseTry(Callable<? extends T> fallback) { return this; }
+
+    @Override
+    public Try<T> recover(Function<Throwable, Optional<T>> f) { return this; }
+
+    @Override
+    public <U> Try<U> map(Function<? super T, ? extends U> f) {
+        return new Success<>(f.apply(get()));
+    }
+
+    @Override
+    public <U> Try<U> flatMap(Function<? super T, Try<U>> f) {
+        return f.apply(get());
+    }
+
+    @Override
+    public String toString() {
+        return "success(" + get() + ")";
+    }
+}
+
+class Failure<T> extends Left<Throwable, T> implements Try<T> {
+
+    public Failure(Throwable thrown) { super(thrown); }
+
+    @Override
+    public T getOrElse(T fallback) { return fallback; }
+
+    @Override
+    public T getOrElse(Supplier<T> fallback) { return fallback.get(); }
+
+    @Override
+    public Try<T> orElse(Try<T> fallback) { return fallback; }
+
+    @Override
+    public Try<T> orElse(Supplier<Try<T>> fallback) { return fallback.get(); }
+
+    @Override
+    public Try<T> orElseTry(Callable<? extends T> fallback) { return Try.tryGet(fallback); }
+
+    @Override
+    public Try<T> recover(Function<Throwable, Optional<T>> f) {
+        Optional<T> recovered = f.apply(getFailure());
+        if(recovered.isPresent()) {
+            return new Success<>(recovered.get());
+        } else {
+            return this;
+        }
+    }
+
+    @Override
+    public <U> Try<U> map(Function<? super T, ? extends U> f) {
+        return new Failure<>(getFailure());
+    }
+
+    @Override
+    public <U> Try<U> flatMap(Function<? super T, Try<U>> f) {
+        return new Failure<>(getFailure());
+    }
+
+    @Override
+    public String toString() {
+        return "failure(" + getFailure() + ")";
+    }
 }

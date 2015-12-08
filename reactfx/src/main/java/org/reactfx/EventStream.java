2578bb68fadb8ad4f85ce5dfe31158afcd62dd3c
JordanMartinez <jordanalex.martinez@gmail.com>
2015-12-07 19:34:06 -0800

    Added a marble diagram that better shows `emitOnEach`'s usage.

--- reactfx/src/main/java/org/reactfx/EventStream.java
+++ reactfx/src/main/java/org/reactfx/EventStream.java
@@ -392,10 +392,21 @@ public interface EventStream<T> extends Observable<Consumer<? super T>> {
     /**
      * Returns a new event stream that, when an event arrives from the
      * {@code impulse} stream, emits the most recent event emitted by this
-     * stream. The same event may be emitted more than once. For example, if
-     * events are emitted in this order, [<i>i, a, i, b, c, i, i, d</i>], where
-     * <i>a, b, c, d</i> come from this stream and <i>i</i>s come from the
-     * {@code impulse} stream, then the returned stream emits [<i>a, c, c</i>].
+     * stream. The same event may be emitted more than once.
+     * For example,
+     * <pre>
+     *     {@code
+     *     EventStream<?> A = ...;
+     *     EventStream<?> B = ...;
+     *     EventStream<?> C = A.emitOnEach(B);}
+     * </pre>
+     * <p>Returns C. When B emits an event, C emits A's most recent event.
+     * <pre>
+     *     Time ---&gt;
+     *     A :-a-------b-----c----------d-------&gt;
+     *     B :----i------------i--i--i-----i----&gt;
+     *     C :----a------------c--c--c-----d----&gt;
+     * </pre>
      */
     default EventStream<T> emitOnEach(EventStream<?> impulse) {
         return new EmitOnEachStream<>(this, impulse);

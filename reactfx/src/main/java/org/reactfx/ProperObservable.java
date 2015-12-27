b23685ab7bc6c985c34b9ad1b7ed1fcd95cd79d5
Tomas Mikula <tomas.mikula@gmail.com>
2015-12-28 00:32:02 +0100

    Fix typo in Javadoc.

--- reactfx/src/main/java/org/reactfx/ProperObservable.java
+++ reactfx/src/main/java/org/reactfx/ProperObservable.java
@@ -7,7 +7,7 @@ import org.reactfx.util.NotificationAccumulator;
  * observers and notifies them when a change occurs. This is unlike
  * {@link ProxyObservable}, which registers observers with an underlying
  * {@linkplain Observable}, and unlike {@link RigidObservable}, which does
- * produce any notifications.
+ * not produce any notifications.
  * @param <O> observer type accepted by this {@linkplain Observable}
  * @param <T> notification type produced by this {@linkplain Observable}
  */
